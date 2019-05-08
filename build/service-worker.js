8d27b009f0cbf1e7db0cbb0de089d9615fcfe5ad
VCITY-PC1\richard.navin <richard.dharmaraj@adabisc.com>
2019-05-08 13:12:48 +0300

    build location path

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.75fa189722f8d21be653207d06d6a519.js"
+  "/ReactTodo/build/precache-manifest.75fa189722f8d21be653207d06d6a519.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ReactTodo/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
