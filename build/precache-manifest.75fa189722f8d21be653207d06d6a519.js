7c747173c18e197b4fa891407d3afdf1fad8547d
VCITY-PC1\richard.navin <richard.dharmaraj@adabisc.com>
2019-05-08 12:21:09 +0300

    Build file

---
+++ build/precache-manifest.75fa189722f8d21be653207d06d6a519.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4cfb379319b4e204aecd",
+    "url": "/static/js/main.4cfb3793.chunk.js"
+  },
+  {
+    "revision": "6f96e78ba8e11d79daff",
+    "url": "/static/js/1.6f96e78b.chunk.js"
+  },
+  {
+    "revision": "4cfb379319b4e204aecd",
+    "url": "/static/css/main.985c7200.chunk.css"
+  },
+  {
+    "revision": "ae11c0bbf997ac7953da8f58db26c1a7",
+    "url": "/index.html"
+  }
+];
